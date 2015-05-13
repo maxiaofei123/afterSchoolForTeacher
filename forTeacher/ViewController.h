decd11da7d87e614897db2a8a269d00709d95834
susu <susu_mm@126.com>
2015-05-13 16:28:27 +0800

    Initial Commit

---
+++ forTeacher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  forTeacher
+//
+//  Created by susu on 15/5/13.
+//  Copyright (c) 2015年 susu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ forTeacherTests/forTeacherTests.m
@@ -0,0 +1,40 @@
+//
+//  forTeacherTests.m
+//  forTeacherTests
+//
+//  Created by susu on 15/5/13.
+//  Copyright (c) 2015年 susu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface forTeacherTests : XCTestCase
+
+@end
+
+@implementation forTeacherTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
